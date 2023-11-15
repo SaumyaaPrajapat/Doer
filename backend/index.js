029a0165a227923e6bfffe474a27dca87f7d2570
Anukanksha <anukanksha.priya2021@vitstudent.ac.in>
2023-11-15 09:56:40 +0530

    tittle change on clicking

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userModel = require("./model/signups");
