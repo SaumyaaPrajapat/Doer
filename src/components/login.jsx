25e9f5d681f61e43d9cf00607708e1e374a41129
Aashay Bothra <aashaybothra89@gmail.com>
2023-11-15 13:11:55 +0530

    Home Page, Login Page and Signup Page responsive frontend update

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
-import backgroundImage from "./img/bgi.jpeg";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import imageArt from "./img/imageart.png";
+import Logo from "./img/Logo.png";
+import { Link } from "react-router-dom";
+import "./login.css";
+
+
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -30,65 +35,92 @@ function Login() {
         console.log(err);
       });
   };
-
-  const cardStyle = {
-    fontFamily: "Poppins",
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    height: "100vh", // Set a minimum height for the card
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+  const cardNew = {
+    width: "75%",
+    paddingTop: "7%"
+  };
+  
+  const buttonStyle = {
+    background: "transparency",
+    color: "#FFF",
+    marginLeft: "10px",
+    fontWeight: "400",
+    fontSize: "18px", 
+    marginTop: "5"
   };
   return (
-    <div style={cardStyle} className="bg-secondary">
-      <div className="mx-auto shadow-lg p-3 rounded-4 px-4" style={{ backgroundColor: "#313641", width: "40%", maxWidth: "370px"}}>
-        <h2 className="d-flex justify-content-center align-items-center text-white">
-          Login
-        </h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <div className="mb-1 text-white">
-              <label htmlFor="email">
-                <strong>E-mail</strong>
-              </label>
+    <div className="cardStyle">
+      <div className="navbarStyle">
+        <div>
+          <img src={Logo} className="logoStyle" alt="Logo" />
+        </div>
+        <div className="buttonContainerStyle">
+          <Link
+            to="/register"
+            className="btn rounded-10"
+            style={buttonStyle}
+          >
+            Get Started
+          </Link>
+          <Link
+            to="/login"
+            className="btn rounded-10"
+            style={buttonStyle}
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+      <div className="blurredCardStyle mx-auto p-3 rounded-4 px-4">
+        <div>
+          <img src={imageArt} alt="" className="art"/>
+        </div>
+        <div style={cardNew}>
+          <h2 className="d-flex justify-content-center align-items-center" style={{color: "#DCDADB"}}>
+            Login
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <div className="mb-1" style={{color: "#DCDADB"}}>
+                <label className="formLabel"htmlFor="email">
+                  <strong>E-mail</strong>
+                </label>
+              </div>
+              <div>
+                <input
+                  type="email"
+                  //placeholder="Enter E-mail"
+                  autoComplete="off"
+                  name="email"
+                  className="inputStyle form-control rounded-3" required
+                  onChange={(e) => setEmail(e.target.value)}
+                  
+                />
+              </div>
             </div>
-            <div>
+            <div className="mb-3">
+              <div className="mb-1" style={{color: "#DCDADB"}}>
+                <label className="formLabel" htmlFor="email">
+                  <strong>Password</strong>
+                </label>
+              </div>
               <input
-                type="email"
-                placeholder="Enter E-mail"
-                autoComplete="off"
-                name="email"
-                className="form-control rounded-3"
-                onChange={(e) => setEmail(e.target.value)}
+                type="password"
+                //placeholder="Enter Password"
+                name="password"
+                className="inputStyle form-control rounded-3"
+                onChange={(e) => setPassword(e.target.value)}
+                
               />
             </div>
-          </div>
-          <div className="mb-3">
-            <div className="mb-1 text-white">
-              <label htmlFor="email">
-                <strong>Password</strong>
-              </label>
+            {error && <p className="text-danger">{error}</p>}
+            <div className="d-flex justify-content-center">
+              <button type="submit" className="btn w-50 rounded-pill" style={{ backgroundColor: "#67BBD3", color: "#FFF", marginTop: "5%" }}>
+                Login
+              </button>
             </div>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              className="form-control rounded-3"
-              onChange={(e) => setPassword(e.target.value)}
-            />
+          </form>
           </div>
-          {error && <p className="text-danger">{error}</p>}
-          <button type="submit" className="btn btn-primary w-100 rounded-10">
-            Login
-          </button>
-          <div className="d-flex justify-content-center">
-            <button type="submit" className="btn w-50 rounded-pill" style={{ backgroundColor: "#67BBD3" }}>
-              Login
-            </button>
-          </div>
-        </form>
       </div>
     </div>
   );
