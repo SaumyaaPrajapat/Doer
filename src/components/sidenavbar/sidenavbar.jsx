36ce25fe0b1fe5cd5c3e67c2f34e33e02adb9633
Saumyaa Prajapat <saumyaa.prajapat@gamil.com>
2023-12-09 22:51:32 +0530

    sidenavbar profile

--- src/components/sidenavbar/sidenavbar.jsx
+++ src/components/sidenavbar/sidenavbar.jsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import DateDisplay from "./DateDisplay";
 import "./sidenavbar.css";
+import { FaUserCircle } from "react-icons/fa";
 
 const Clock = () => {
   let time = new Date().toLocaleTimeString();
@@ -17,6 +18,26 @@ const Clock = () => {
 };
 
 const SideNavbar = ({ onClose }) => {
+  const [name, setUserName] = useState("");
+  const [email, setUserEmail] = useState("");
+
+  useEffect(() => {
+    const storedName = sessionStorage.getItem("name");
+    const storedEmail = sessionStorage.getItem("email");
+
+    if (storedName) {
+      setUserName(capitalizeFirstLetter(storedName));
+    }
+    if (storedEmail) {
+      setUserEmail(storedEmail);
+    }
+  }, []);
+
+  // Function to capitalize the first letter of a string
+  const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
   return (
     <div className="side-navbar-container open">
       <div className="close-icon" onClick={onClose}>
@@ -27,6 +48,28 @@ const SideNavbar = ({ onClose }) => {
       </div>
       <div className="side-date">
         <DateDisplay />
+      </div>
+      <div className="procontainer">
+        <h3 className="text-center">Profile</h3>
+        <form className="proform">
+          <div className="circle-container">
+            <div className="circle">
+              <FaUserCircle className="profile" />
+            </div>
+          </div>
+          <div className="progroup">
+            <label htmlFor="name" className="form-label">
+              <strong>Name:</strong>
+            </label>
+            <h3 className="pro">{name || "user"}</h3>
+          </div>
+          <div className="progroup">
+            <label htmlFor="description" className="form-label">
+              <strong>Email:</strong>
+            </label>
+            <h3 className="pro">{email || "userEmail"}</h3>
+          </div>
+        </form>
       </div>
     </div>
   );
