69dee197fa5db3acaba2acf62997abfe2661173d
Saumyaa Prajapat <saumyaa.prajapat@gamil.com>
2023-12-13 23:39:17 +0530

    footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Made with 🩶 by Team AS</p>
+    </footer>
+  );
+};
+
+export default Footer;
